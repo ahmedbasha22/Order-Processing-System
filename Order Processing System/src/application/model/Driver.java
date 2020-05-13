f5494aded0a98d168858475519dedd58d11b3178
M2 <mhmdalaa333@gmail.com>
2020-05-13 20:22:22 +0200

    add SQLException to functions driver interface

--- Order Processing System/src/application/model/Driver.java
+++ Order Processing System/src/application/model/Driver.java
@@ -1,58 +1,59 @@
 package application.model;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public interface Driver {
+public interface Driver  {
 
-	public boolean AlreadyRegistredUsername(String username);
+	public boolean AlreadyRegistredUsername(String username) throws SQLException;
 	
-	public boolean AlreadyRegistredEmail(String email);
+	public boolean AlreadyRegistredEmail(String email) throws SQLException;
 		
-	public void addNewUser(User newUser);
+	public void addNewUser(User newUser) throws SQLException;
 	
-	public User getUser(String username, String password);
+	public User getUser(String username, String password) throws SQLException;
 
-	public boolean validateUser(String username, String password);
+	public boolean validateUser(String username, String password) throws SQLException;
 
-	public User modifyExistingUser(String oldUsername, String oldPassword, User newUserInfo);
+	public User modifyExistingUser(String oldUsername, String oldPassword, User newUserInfo) throws SQLException;
 	
-	public void promoteUser(String userName);
+	public void promoteUser(String userName) throws SQLException;
 	
-	public void addNewBook(Book newBook, int minimumQuantity);
+	public void addNewBook(Book newBook, int minimumQuantity) throws SQLException;
 	
-	public Book modifyExistingBook(int oldISBN, Book newBookInfo, Integer newMinimumQuantity);
+	public Book modifyExistingBook(int oldISBN, Book newBookInfo, Integer newMinimumQuantity) throws SQLException;
 	
-	public Book orderMoreQuantity(int ISBN, int addedQuantity);
+	public Book orderMoreQuantity(int ISBN, int addedQuantity) throws SQLException;
 	
-	public List<Book> getBooksByISBN(int ISBN);
+	public List<Book> getBooksByISBN(int ISBN) throws SQLException;
 	
-	public List<Book> getBooksByTitle(String title);
+	public List<Book> getBooksByTitle(String title) throws SQLException;
 	
-	public List<Book> getBooksByAuthor(String author);
+	public List<Book> getBooksByAuthor(String author) throws SQLException;
 	
-	public List<Book> getBooksBySellingPrice(int sellingPrice);
+	public List<Book> getBooksBySellingPrice(int sellingPrice) throws SQLException;
 	
-	public List<Book> getBooksByCatgory(String category);
+	public List<Book> getBooksByCatgory(String category) throws SQLException;
 
-	public List<Book> getBooksByPublicationYear(int publicationYear);
+	public List<Book> getBooksByPublicationYear(int publicationYear) throws SQLException;
 	
-	public List<Book> getBooksByPublisherName(String publisherName);
+	public List<Book> getBooksByPublisherName(String publisherName) throws SQLException;
 	
-	public List<Book> addBookToShoppingCart(String username, int bookISBN, int quantity);
+	public List<Book> addBookToShoppingCart(String username, int bookISBN, int quantity) throws SQLException;
 	
-	public List<Book> modifyBookInShoppingCart(String username, int bookISBN, int quantity);
+	public List<Book> modifyBookInShoppingCart(String username, int bookISBN, int quantity) throws SQLException;
 
-	public List<Book> removeBookFromShppingCart(String username, int bookISBN);
+	public List<Book> removeBookFromShppingCart(String username, int bookISBN) throws SQLException;
 	
-	public void checkoutShoppingCart(String username);
+	public void checkoutShoppingCart(String username) throws SQLException;
 
-	public List<Book> getBookSalesPreviousMonth();
+	public List<Book> getBookSalesPreviousMonth() throws SQLException;
 	
-	public List<User> getTop5Users();
+	public List<User> getTop5Users() throws SQLException;
 	
-	public List<Book> getUserPurchases(String username);
+	public List<Book> getUserPurchases(String username) throws SQLException;
 	
-	public List<Book> getTop10SoldBooks();
+	public List<Book> getTop10SoldBooks() throws SQLException;
 	
-	public void close();
+	public void close() throws SQLException;
 }
