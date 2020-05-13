806c58658fd18052aeecf92291eb50e2122db813
Ahmed <aabasha22@gmail.com>
2020-05-13 05:16:34 +0200

    Login window is ready.

--- Order Processing System/src/application/Main.java
+++ Order Processing System/src/application/Main.java
@@ -1,17 +1,31 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
+			Parent root;
+
+			try {
+			    root = loader.load();
+			} catch (IOException ioe) {
+			    // log exception
+			    return;
+			}
+
+			Scene scene = new Scene(root,775,513);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
