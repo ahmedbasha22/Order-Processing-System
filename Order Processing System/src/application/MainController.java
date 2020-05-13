c64c1490ab7e9e0b1e1970e5ef403b461a9e1f36
Ahmed <aabasha22@gmail.com>
2020-05-13 16:54:22 +0200

    Sign-up is added with navigation.

---
+++ Order Processing System/src/application/MainController.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainController {
+		
+	public void goSignup(ActionEvent event) throws IOException {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Signup.fxml"));
+			Parent root;
+
+			try {
+			    root = loader.load();
+			} catch (IOException ioe) {
+			    // log exception
+			    return;
+			}
+
+			Scene scene = new Scene(root,775,513);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Stage window = (Stage)(((Node)event.getSource()).getScene().getWindow());
+			window.setScene(scene);
+			window.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void goLogin(ActionEvent event) throws IOException {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
+			Parent root;
+
+			try {
+			    root = loader.load();
+			} catch (IOException ioe) {
+			    // log exception
+			    return;
+			}
+
+			Scene scene = new Scene(root,775,513);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Stage window = (Stage)(((Node)event.getSource()).getScene().getWindow());
+			window.setScene(scene);
+			window.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+}
