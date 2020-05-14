518a4d3a9b6c5a714ffd78455242815644d472d9
Ahmed <aabasha22@gmail.com>
2020-05-15 00:27:10 +0200

    Books Warehouse is added with GUI (sellingPrice have not been read yet)

--- Order Processing System/src/application/MainController.java
+++ Order Processing System/src/application/MainController.java
@@ -1,17 +1,33 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
+import application.model.Book;
+import application.model.Publisher;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class MainController {
-		
+
+	
 	public void goSignup(ActionEvent event) throws IOException {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("Signup.fxml"));
@@ -59,7 +75,7 @@ public class MainController {
 			e.printStackTrace();
 		}
 	}
-	
-	
 
+	
+	
 }
